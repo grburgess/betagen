d77bbad9fb7c0a5d4c08e1703c5fc38bcf2d6f19
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-11-24 22:49:39 +0100

    updates

--- betagen/__init__.py
+++ betagen/__init__.py
@@ -1,7 +1,8 @@
-
-
+import matplotlib.pyplot as plt
 from colour import Color
 from betagen.utils import colorscale
+
+
 def betagen(base_dark):
     
     HL_scale = .85
@@ -64,3 +65,35 @@ def betagen(base_dark):
 
 
     return dark, dark_highlight, mid, mid_highlight, light, light_highlight
+
+
+def display(base_dark):
+
+    dark, dark_highlight, mid, mid_highlight, light, light_highlight = betagen(base_dark)
+
+
+    x = [dark, mid, light]
+    xh = [dark_highlight, mid_highlight, light_highlight]
+            
+    fig, ax = plt.subplots()
+
+    for i, c, in enumerate(xh):
+
+        ax.hlines(i,0,1, colors=c, linewidth=18)
+        ax.hlines(i,0.1,.9, colors=x[i], linewidth=10)
+
+    ax.set_ylim(-1, len(x))
+    _despine(ax,True)
+
+
+def _despine(ax, all=False):
+    if all is True:
+        for sp in ax.spines:
+            ax.spines[sp].set_visible(False)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    else:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
